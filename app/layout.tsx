1de9c4dc1df4deb514ee46d186d83e3749869457
Zeeyeah <zu0827992@gmail.com>
2024-11-21 02:13:37 +0530

    Web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 import { Montserrat } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const wulkanDisplay = localFont({
   src: './fonts/wulkan-display/WulkanDisplayMedium.woff',
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.variable} ${wulkanDisplay.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
